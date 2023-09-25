6d1604ccdecd0e527fbb32823417ea8dd5421e7f
Yedidia AGNIMO <ayedidyas@gmail.com>
2023-09-25 16:27:30 +0200

    feat: add command-line interface for variant_analyzer module.
    * src/stemmabench/cli.py
    - Add `analyze_tradition` CLI for variant analyzer.
    
    * src/stemmabench/utils.py
    - Add utility functions for the `analyze_tradition` command.
    
    * src/stemmabench/variant_analyzer.py
    - Change default arguments type for consistency in CLI.
    
    * pyproject.toml
    - Update project.scripts. Define `stemmabench` as first level command,
    while generate-tradition, and analyze-tradition are the subcommands.
    
    * demo/config.yaml
    - fix: update config file for accounting for fragmentation.
    
    * demo/input_folder
    - Add new files to use for the demo of `analyze-tradition` CLI.
    
    * README.md
    - Minor formatting changes.

--- src/stemmabench/utils.py
+++ src/stemmabench/utils.py
@@ -1,10 +1,14 @@
 """Utility Functions
 """
+import os
+import csv
 from typing import Dict, List
+from pathlib import Path
+
+import numpy as np
 from stemmabench.config_parser import StemmaBenchConfig
 
 
-# Define the configuration of the stemma.
 def make_stemmabench_config(
     depth: int=1,
     width_min: int=2,
@@ -20,8 +24,9 @@ def make_stemmabench_config(
     omit_rate: float=0.01,
     language: str="en"
 ):
-    """Shortcut function to specify a configuration for generating a stemma. 
+    """Shortcurt function to build a configuration file for stemma generation.
     """
+
     config = StemmaBenchConfig(**{
         "meta": {
           "language": language
@@ -74,6 +79,7 @@ def make_stemmabench_config(
             }
         }
     })
+
     return config
 
 
@@ -93,6 +99,33 @@ def capitalize_sentences(text: str, sep=".") -> str:
     ])).strip()
 
 
+def load_tradition(input_folder: str, sep: str = ".") -> Dict[str, str]:
+    """
+    Load tradition from input folder and format it.
+
+    Args:
+        input_folder (str): The input folder containing witness text files.
+        sep (str, optional): Sentence separator. Defaults to ".".
+
+    Returns:
+        Dict[str, str]: A dictionary with witness names as keys and 
+            formatted witness content as values.
+    """
+    tradition = {}
+    # List directory files/
+    for filename in os.listdir(input_folder):
+        # Select .txt files.
+        if filename.endswith(".txt"):
+            # Get file name (remove .txt extension).
+            witness_name = os.path.splitext(filename)[0]
+            # Add file content to the dictionary.
+            with open(os.path.join(input_folder, filename), "r", encoding="utf-8") as file:
+                witness_content = file.read()
+                formatted_content = capitalize_sentences(witness_content, sep=sep)
+                tradition[witness_name] = formatted_content
+    return tradition
+
+
 def format_tradition(tradition: Dict[str, str]) -> Dict[str, List[Dict[str, str]]]:
     """Format tradition to meet the input requirements for collatex collation.
 
@@ -107,3 +140,43 @@ def format_tradition(tradition: Dict[str, str]) -> Dict[str, List[Dict[str, str]
     return {"witnesses": [{"id": witness_id, "content": content}
                             for witness_id, content in tradition.items()]
             }
+
+
+def save_analysis_summary_to_csv(analysis_summary: Dict[str, float],
+                                 output_path: str):
+    """Save an analysis summary result to a csv file.
+    The input is a dictionary that look like the ouput of the method 
+    `VariantAnalyzer.analysis_summary()`.
+
+    Args:
+        analysis_summary (Dict[str, float]): The dictionary to save.
+        output_path (str): The filepath where to save the dictionary.
+    """
+    if not output_path.endswith(".csv"):
+        output_path = Path(output_path + ".csv")
+    output_path = Path(output_path)
+    with open(output_path, "w", newline="", encoding="utf-8") as csvfile:
+        writer = csv.writer(csvfile)
+        writer.writerow(["Variant Type", "Rate"])
+        for variant_type, rate in analysis_summary.items():
+            writer.writerow([variant_type, rate])
+
+
+def save_matrix_to_csv(matrix: np.ndarray,
+                       output_path: str):
+    """Save a matrix to csv.
+
+    Args:
+        matrix (np.ndarray): _description_
+        output_path (str): _description_
+
+    Raises:
+        AttributeError: _description_
+    """
+    if not output_path.endswith(".csv"):
+        output_path = Path(output_path + ".csv")
+    output_path = Path(output_path)
+    with open(output_path, "w", newline="", encoding="utf-8") as csvfile:
+        writer = csv.writer(csvfile)
+        for row in matrix:
+            writer.writerow(row)
